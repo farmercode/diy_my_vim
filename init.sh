f4b6bed9b48be20471f7f5d9da531db6091bd0b8
farmercode <wangchangchun120@gmail.com>
2016-09-30 10:13:37 +0800

    安装vundle插件，移动配置玩呢间

--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#
+
+vundle_dir=~/.vim/bundle/vundle
+if [ -d $vundle_dir ];then
+	echo "installing vundle.Please wait."
+	git clone https://github.com/gmarik/vundle.git $vundle_dir
+
+fi
+
+cp vimrc_config ~/.vimrc
