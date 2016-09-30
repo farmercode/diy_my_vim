f7e22ce466a14a0342979ba7778ea3e620d20734
farmercode <wangchangchun120@gmail.com>
2016-09-30 11:03:22 +0800

    安装ctags

--- init.sh
+++ init.sh
@@ -2,10 +2,14 @@
 #
 
 vundle_dir=~/.vim/bundle/vundle
-if [ -d $vundle_dir ];then
+if [ ! -d $vundle_dir ];then
 	echo "installing vundle.Please wait."
 	git clone https://github.com/gmarik/vundle.git $vundle_dir
 
 fi
 
 cp vimrc_config ~/.vimrc
+#安装ctags
+yum install ctags
+
+vim +BundleInstall +qall
