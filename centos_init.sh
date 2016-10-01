e1118fe54f7bb3457052226e49f4edb66e4c874f
farmercode <wangchangchun120@gmail.com>
2016-10-01 13:00:10 +0800

    手动下载YouCompleteMe插件

--- centos_init.sh
+++ centos_init.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #
 #安装依赖软件包
-yum install ctags cmake clang
+yum -y install ctags cmake clang python
 
 vundle_dir=~/.vim/bundle/vundle
 if [ ! -d $vundle_dir ];then
 	echo "installing vundle.Please wait."
 	git clone https://github.com/gmarik/vundle.git $vundle_dir
 
+fi
+#下载YoucompleteMe插件，使用vundle安装时总是失败
+YoucompleteMeInstallFile=$vundle_dir"/YouCompleteMe/install.py"
+if [[ ! -f $YoucompleteMeInstallFile ]]; then
+    git clone https://github.com/Valloric/YouCompleteMe.git $vundle_dir
+    pushd $vundle_dir"/YouCompleteMe"
+    git submodule update --init --recursive
+    $YoucompleteMeInstallFile --clang-completer
 fi
 
 cp vimrc_config ~/.vimrc
