b60c96bfd4a3d6bba931aa8500532d6907db6ba2
farmercode <wangchangchun120@gmail.com>
2016-10-01 13:09:36 +0800

    bug fix

--- centos_init.sh
+++ centos_init.sh
@@ -2,8 +2,8 @@
 #
 #安装依赖软件包
 yum -y install ctags cmake clang python
-
-vundle_dir=~/.vim/bundle/vundle
+vim_plugin_dir=~/.vim/bundle
+vundle_dir=$vim_plugin_dir"/vundle"
 if [ ! -d $vundle_dir ];then
 	echo "installing vundle.Please wait."
 	git clone https://github.com/gmarik/vundle.git $vundle_dir
@@ -13,10 +13,10 @@ fi
 cp vimrc_config ~/.vimrc
 vim +BundleInstall +qall
 #下载YoucompleteMe插件，使用vundle安装时总是失败
-YoucompleteMeInstallFile=$vundle_dir"/YouCompleteMe/install.py"
+YoucompleteMeInstallFile=$vim_plugin_dir"/YouCompleteMe/install.py"
 if [[ ! -f $YoucompleteMeInstallFile ]]; then
-    git clone https://github.com/Valloric/YouCompleteMe.git $vundle_dir
-    pushd $vundle_dir"/YouCompleteMe"
-    git submodule update --init --recursive
-    $YoucompleteMeInstallFile --clang-completer
+    git clone https://github.com/Valloric/YouCompleteMe.git $vim_plugin_dir
 fi
+pushd $vim_plugin_dir"/YouCompleteMe"
+git submodule update --init --recursive
+$YoucompleteMeInstallFile --clang-completer
